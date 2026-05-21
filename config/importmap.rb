9c4f61488a8544b58c3c568219796ae0224de7da
Julien Chardon <julien@MacBook-Air-de-Julien.local>
2026-05-21 16:43:15 +0200

    chore: bootstrap Rails 8 app with PostgreSQL, Tailwind, Chartkick, RSpec

---
+++ config/importmap.rb
@@ -0,0 +1,5 @@
+# Pin npm packages by running ./bin/importmap
+
+pin "application"
+pin "chartkick", to: "https://cdn.jsdelivr.net/npm/chartkick@5/dist/chartkick.esm.js"
+pin "chart.js", to: "https://cdn.jsdelivr.net/npm/chart.js@4/dist/chart.umd.js"
